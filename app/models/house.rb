7ba322e34659fe937ffe7b861ce94627a0ec2a56
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-20 18:11:10 +0500

    Check linters

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,4 +1,4 @@
-class House < ApplicationRecord 
+class House < ApplicationRecord
   belongs_to :user
   has_many :reservations, dependent: :destroy
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,6 @@
 class Reservation < ApplicationRecord
-    belongs_to :user
-    belongs_to :house
-    validates :user, presence: true
-    validates :house, presence: true
+  belongs_to :user
+  belongs_to :house
+  validates :user, presence: true
+  validates :house, presence: true
 end
