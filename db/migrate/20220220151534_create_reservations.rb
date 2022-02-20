a5d2628eccf8de019d9e3bbbb435f34945022398
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-21 00:05:27 +0500

    Update Migrations

--- db/migrate/20220220151534_create_reservations.rb
+++ db/migrate/20220220151534_create_reservations.rb
@@ -2,7 +2,7 @@ class CreateReservations < ActiveRecord::Migration[6.1]
   def change
     create_table :reservations do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :houses, null: false, foreign_key: true
+      t.references :house, null: false, foreign_key: true
       t.datetime :start_time, null: false
       t.datetime :end_time, null: false
       t.timestamps
