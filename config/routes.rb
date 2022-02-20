1370aeee097a742deeb66c6e6491c3f61acca752
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-20 18:49:34 +0500

    Saving files before refreshing line endings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :v1 do 
+    resources :houses, only: [:index, :show, :create, :destroy]
+    resources :reservations, only: [:index, :show, :create, :destroy]
+  end
 end
