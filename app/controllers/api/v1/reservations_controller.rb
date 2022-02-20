194cd4c2f687a74239e63ea3d98b29a5aed9459b
kossi-stack <kossifioklou10@gmail.com>
2022-02-20 20:23:45 +0100

    Create method for reservations controller

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -3,4 +3,12 @@ class Api::V1::ReservationsController < ApplicationController
       @reservations = current_user.reservations
       render json: @reservations
     end
+    def create
+        @reservation = current_user.reservations.new(reservation_params)
+        if @reservation.save
+          render json: @reservation
+        else
+          render json: { errors: @reservation.errors.full_messages }, status: :unprocessable_entity
+        end
+    end    
 end  
