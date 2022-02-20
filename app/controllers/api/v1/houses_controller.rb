9a0b290d7c459e899d00d6599d4a9567e7bced3a
kossi-stack <kossifioklou10@gmail.com>
2022-02-20 20:18:13 +0100

    Create method for houses controller

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -3,4 +3,12 @@ class Api::V1::HousesController < ApplicationController
       @houses = House.all
       render json: @houses
     end
+    def create
+        @house = current_user.houses.new(house_params)
+        if @house.save
+          render json: @house
+        else
+          render json: { errors: @house.errors.full_messages }, status: :unprocessable_entity
+        end
+    end    
 end
