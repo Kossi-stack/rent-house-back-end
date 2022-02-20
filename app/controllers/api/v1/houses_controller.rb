5812ddd50836215e63b6c8b674da36e2674978df
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-21 00:27:55 +0500

    Check linters

--- app/controllers/api/v1/houses_controller.rb
+++ app/controllers/api/v1/houses_controller.rb
@@ -1,22 +1,25 @@
 class Api::V1::HousesController < ApplicationController
-    def index
-      @houses = House.all
-      render json: @houses
+  def index
+    @houses = House.all
+    render json: @houses
+  end
+
+  def create
+    @house = current_user.houses.new(house_params)
+    if @house.save
+      render json: @house
+    else
+      render json: { errors: @house.errors.full_messages }, status: :unprocessable_entity
     end
-    def create
-        @house = current_user.houses.new(house_params)
-        if @house.save
-          render json: @house
-        else
-          render json: { errors: @house.errors.full_messages }, status: :unprocessable_entity
-        end
-    end   
-    def destroy
-        @house = House.find(params[:id])
-        @house.destroy
-        render json: @house
-    end
-    private
+  end
+
+  def destroy
+    @house = House.find(params[:id])
+    @house.destroy
+    render json: @house
+  end
+
+  private
 
   def house_params
     params.require(:house).permit(:name, :rooms, :beds, :baths, :price, :rating, :image_url, :description)
