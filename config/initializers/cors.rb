7b48d197eb81ac52ddf09e89f6f55decba879dce
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-22 21:09:45 +0500

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'http://127.0.0.1:3001'
-
-    resource '*',
+    origins 'https://sleepy-depths-68420.herokuapp.com/ ', 'http://localhost:3001', 'http://127.0.0.1:3001'
     resource '*', headers: %w(Authorization), expose: %w(Authorization), methods: :any, max_age: 600 
   end
 end
