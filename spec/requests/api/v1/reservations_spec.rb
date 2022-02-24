29c234f3c7a34e57726861deaf8207faa4f47050
kossi-stack <kossifioklou10@gmail.com>
2022-02-24 01:42:23 +0100

    Add test for request reservation

---
+++ spec/requests/api/v1/reservations_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe "Api::V1::Reservations", type: :request do
+
+  before do
+    visit user_session
+    fill_in 'Name', with: 'Test'
+    fill_in 'Email', with: 'donard@gmail.com'
+    fill_in 'Password', with: 'password'
+    click_button 'Sign up'
+  end
+
+  describe "GET /api/v1/reservations" do
+    it "works! (now write some real specs)" do
+      get '/api/v1/reservations'
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe 'POST /create' do
+    it 'create a new reservations' do
+      post '/api/v1/reservations?user_id=1&house_id=1'
+      expect(response.body).to include('user_id')
+    end
+  end
+end
