a9ebe73e74f26904aed26e6c42cefa6edc3e3d45
kossi-stack <kossifioklou10@gmail.com>
2022-02-24 12:12:09 +0100

    Add factories spec for reservation

--- spec/factories/house_factorie.rb
+++ spec/factories/house_factorie.rb
@@ -1,3 +1,12 @@
 FactoryBot.define do 
-
+  factory :house do
+    name { 'Bugos' }
+    rooms { 'Begos' }
+    beds { 'Bugos' }
+    baths { 'Begos' }
+    price { 'Bugos' }
+    rating { 'Begos' }
+    image_url { 'Bigos' }
+    user_id { '1' }
+  end
 end
