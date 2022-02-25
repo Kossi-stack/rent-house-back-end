b03a49d123a1c4844fccb222d4edb01847d600b2
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-25 16:57:37 +0500

    Update the cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
-    resource "*", headers: %w(Authorization), expose: %w(Authorization), methods: :any, max_age: 600
+    origins '*'
+    resource '*', headers: %w(Authorization), expose: %w(Authorization), methods: :any, max_age: 600 
   end
 end
