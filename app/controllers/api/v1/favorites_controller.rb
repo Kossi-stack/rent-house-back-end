77183803efeebf441f264218463e7a4abae75a18
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-25 16:47:51 +0500

    Update favorites settings

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::FavoritesController < ApplicationController
+  before_action :set_favorite, only: [:destroy]
+
   def index
     @favorites = current_user.favorites
     render json: @favorites
@@ -14,12 +16,18 @@ class Api::V1::FavoritesController < ApplicationController
   end
 
   def destroy
-    @favorite = Favorite.find(params[:id])
-    @favorite.destroy
-    render json: @favorite
+    if @favorite.destroy
+      render json: { message: 'Favorite deleted' }
+    else
+      render json: { errors: @favorite.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   private
+
+  def set_favorite
+    @favorite = Favorite.find(params[:id])
+  end
 
   def favorite_params
     params.require(:favorite).permit(:house_id)
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,6 @@
 class House < ApplicationRecord
   belongs_to :user, optional: true
   has_many :reservations, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   validates :name, presence: true
 end
