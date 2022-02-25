82c4f0031c869f9682216942fb426a5155ff8bb2
Rustam <69011963+Rustamxon7@users.noreply.github.com>
2022-02-25 05:53:21 +0500

    Create Favorite section

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
 
   has_many :houses, dependent: :destroy
   has_many :reservations, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :email, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_20_151534) do
+ActiveRecord::Schema.define(version: 2022_02_25_005002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "house_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["house_id"], name: "index_favorites_on_house_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "houses", force: :cascade do |t|
     t.string "name", null: false
@@ -53,6 +62,8 @@ ActiveRecord::Schema.define(version: 2022_02_20_151534) do
     t.index ["jti"], name: "index_users_on_jti", unique: true
   end
 
+  add_foreign_key "favorites", "houses"
+  add_foreign_key "favorites", "users"
   add_foreign_key "houses", "users"
   add_foreign_key "reservations", "houses"
   add_foreign_key "reservations", "users"
